1d72918301af710d991aa5c2291ad42339007a0d
Nelson Muquissi <emelsonmuquissi@gmail.com>
2025-07-12 18:22:09 +0100

    New methods for API, GET and POST

--- api_rest/views.py
+++ api_rest/views.py
@@ -30,3 +30,37 @@ def get_by_nick(request, nick):
     if request.method == 'GET':
         serializer = UserSerializer(user)
         return Response(serializer.data)
+    
+
+#CRUD
+@api_view(['GET', 'POST', 'PUT', 'DELETE'])
+def user_manager(request):
+
+    # Get method
+    if request.method == 'GET':
+        try:
+            if request.GET['user']:
+                user_nickname = request.GET['user']
+
+                try:
+                    user = User.objects.get(pk=user_nickname)
+                except:
+                    return Response(status=status.HTTP_404_NOT_FOUND)
+
+                seralizer = UserSerializer(user)
+                return Response(seralizer.data)
+            else:
+                return Response(status=status.HTTP_400_BAD_REQUEST)
+        except:
+            return Response(status=status.HTTP_400_BAD_REQUEST)
+        
+    if request.method == "POST":
+        new_user = request.data
+
+        seralizer = UserSerializer(data=new_user)
+
+        if seralizer.is_valid():
+            seralizer.save()
+            return Response(seralizer.data, status=status.HTTP_201_CREATED)
+        return Response(status=status.HTTP_400_BAD_REQUEST)
+
